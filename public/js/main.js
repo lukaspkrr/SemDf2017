25c448058ca04cc3ab95106946cb463817dbc68e
Munhoz <munhozfontana@gmail.com>
2017-11-25 18:18:44 -0200

    Mapa
    Configurando mapa, e consumo de dados do mapa

--- public/js/main.js
+++ public/js/main.js
@@ -2,24 +2,26 @@ angular.module('alurapic', ['minhasDiretivas','ngAnimate', 'ngRoute', 'ngResourc
 	.config(function($routeProvider, $locationProvider, $httpProvider) {
 
 		/*$httpProvider.interceptors.push('tokenInterceptor');*/
-    
+
         $locationProvider.html5Mode(true);
-		
+
 		$routeProvider.when('/inicio', {
 			templateUrl: 'partials/principal.html'
 		});
-    
+
             $routeProvider.when('/map', {
-			templateUrl: 'partials/pag-map.html'
-		});
-    
+							templateUrl: 'partials/map.html',
+							controller: 'mapController'
+
+						});
+
             $routeProvider.when('/help', {
             templateUrl: 'partials/help.html'
         });
-    
+
         $routeProvider.when('/formulario/:id', {
             templateUrl: 'partials/formulario.html'
         });
-    
+
 		$routeProvider.otherwise({redirectTo: '/inicio'});
 	});
